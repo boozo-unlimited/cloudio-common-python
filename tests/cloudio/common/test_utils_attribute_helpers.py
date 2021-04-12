ea6156324f24abf8159da934d62cced8f4818516
Thomas Sterren <thomas.sterren@gmail.com>
2021-04-12 14:32:20 +0200

    Attribute helper tests

--- tests/cloudio/common/test_utils_attribute_helpers.py
+++ tests/cloudio/common/test_utils_attribute_helpers.py
@@ -14,6 +14,37 @@ class TestCloudioCommonUtilsAttributeHelpers(unittest.TestCase):
     """Tests attribute_helpers module.
     """
 
+    def test_generate_attribute_names_by_name(self):
+        attribute_name = 'power'
+        attribute_names = attribute_helpers.generate_attribute_names_by_name(attribute_name)
+        self.assertTrue(isinstance(attribute_names, tuple))
+        self.assertTupleEqual(attribute_names, ('_power', 'power'))
+
+        attribute_name = 'powerForMoreFreedom'
+        attribute_names = attribute_helpers.generate_attribute_names_by_name(attribute_name)
+        self.assertTrue(isinstance(attribute_names, tuple))
+        self.assertTupleEqual(attribute_names, ('_power_for_more_freedom', 'power_for_more_freedom'))
+
+        attribute_name = '_powerForMoreFreedom'
+        attribute_names = attribute_helpers.generate_attribute_names_by_name(attribute_name)
+        self.assertTrue(isinstance(attribute_names, tuple))
+        self.assertTupleEqual(attribute_names, ('_power_for_more_freedom', 'power_for_more_freedom'))
+
+        attribute_name = 'flowers_and_bees'
+        attribute_names = attribute_helpers.generate_attribute_names_by_name(attribute_name)
+        self.assertTrue(isinstance(attribute_names, tuple))
+        self.assertTupleEqual(attribute_names, ('_flowers_and_bees', 'flowers_and_bees'))
+
+        attribute_name = 'trees-and-flowers'
+        attribute_names = attribute_helpers.generate_attribute_names_by_name(attribute_name)
+        self.assertTrue(isinstance(attribute_names, tuple))
+        self.assertTupleEqual(attribute_names, ('_trees_and_flowers', 'trees_and_flowers'))
+
+    def test_generate_attribute_names_by_name_coverage(self):
+        attribute_name = ''
+        attribute_names = attribute_helpers.generate_attribute_names_by_name(attribute_name)
+        self.assertTupleEqual(attribute_names, tuple())
+
     def test_generate_setters_from_attribute_name(self):
         attribute_name = 'power'
         setter_method_names = attribute_helpers.generate_setters_from_attribute_name(attribute_name)
@@ -34,3 +65,8 @@ class TestCloudioCommonUtilsAttributeHelpers(unittest.TestCase):
         setter_method_names = attribute_helpers.generate_setters_from_attribute_name(attribute_name)
         self.assertTrue(isinstance(setter_method_names, tuple))
         self.assertTupleEqual(setter_method_names, ('set_power_for_more_freedom', 'setPowerForMoreFreedom'))
+
+    def test_generate_setters_from_attribute_name_coverage(self):
+        attribute_name = ''
+        attribute_names = attribute_helpers.generate_setters_from_attribute_name(attribute_name)
+        self.assertTupleEqual(attribute_names, tuple())
